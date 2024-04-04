1e8c47eafc32c520ff3d27bd944d76af853138ad
TIMUR <kapralov_timur@mail.ru>
2024-04-04 18:13:54 +0300

    SS

--- data/users.py
+++ data/users.py
@@ -16,7 +16,7 @@ class User(SqlAlchemyBase, UserMixin):
     clas = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
-    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
 
