0a398b8993ed4ae03360872f0fcb9fd1e0ad51d2
TIMUR <kapralov_timur@mail.ru>
2024-04-04 18:41:43 +0300

    SSS

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data.login import LoginForm
 from forms.user import RegisterForm
 from data.users import User
 from data import db_session
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -17,6 +17,11 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
+@app.route('/')
+def START():
+    return render_template('base.html')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -25,13 +30,14 @@ def login():
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            return redirect("/")
+            return redirect("/sucsess")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
-
-
+@app.route('/sucsess', methods=['GET', 'POST'])
+def sucsess():
+    pass
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
     formm = RegisterForm()
@@ -56,6 +62,13 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=formm)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
